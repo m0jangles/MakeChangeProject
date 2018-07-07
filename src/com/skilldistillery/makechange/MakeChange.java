6b3d2787b85cb1d13efa0d01bf7e17f50d520c47
Amber Moreno <amber.e.moreno@gmail.com>
2018-07-06 19:04:35 -0600

    first working draft

--- src/com/skilldistillery/makechange/MakeChange.java
+++ src/com/skilldistillery/makechange/MakeChange.java
@@ -7,41 +7,126 @@ public class MakeChange {
 
 		Scanner sc = new Scanner(System.in);
 
-//	User Story #1
-//	The user is prompted asking for the price of the item.
 		double price = 0;
 		double amtTendered = 0;
 		System.out.println("How much is the item? > ");
 		price = sc.nextDouble();
 
-//	User Story #2
-//	The user is then prompted asking how much money was tendered by the customer.
-		System.out.println("How much money did the customer provide? > ");
+		System.out.println("What was the amount tendered? > ");
 		amtTendered = sc.nextDouble();
 
-//	User Story #3
-//	Display an appropriate message if the customer provided too 
-//	little money or the exact amount.
 		if (amtTendered == price) {
-			System.out.println("This is the exact amount of the item");
+			System.out.println("You have provided exact payment");
 		} else if (amtTendered < price) {
 			System.out.println("You need more money for this item");
-		} else if (amtTendered > price) {
-//			make a method to make change
 		}
 
+		int dollars = 0;
+		double coins = 0, change = 0;
+
+		if (amtTendered > price) {
+			change = amtTendered - price;
+			dollars = (int) (change);
+			coins = (amtTendered - dollars) - price;
+			coins = (coins * 100);
+			coins = (int) (coins + .5);
+
+		}
+
+		int hundred = 0, fifty = 0, twenty = 0, ten = 0, five = 0, one = 0;
+		while (dollars > 0) {
+			if (dollars >= 100) {
+				hundred += 1;
+				dollars -= 100;
+			} else if (dollars >= 50) {
+				fifty += 1;
+				dollars -= 50;
+			} else if (dollars >= 20) {
+				twenty += 1;
+				dollars -= 20;
+			} else if (dollars >= 10) {
+				ten += 1;
+				dollars -= 10;
+			} else if (dollars >= 5) {
+				five += 1;
+				dollars -= 5;
+			} else if (dollars >= 1) {
+				one += 1;
+				dollars -= 1;
+			}
+		}
+
+		int penny = 0, nickel = 0, dime = 0, quarter = 0;
+		while (coins > 0) {
+			if (coins >= 25) {
+				quarter += 1;
+				coins -= 25;
+			} else if (coins >= 10) {
+				dime += 1;
+				coins -= 10;
+			} else if (coins >= 5) {
+				nickel += 1;
+				coins -= 5;
+			} else if (coins >= 1) {
+				penny += 1;
+				coins -= 1;
+			} else if (coins < 1) {
+				break;
+			}
+		}
+
+		String bills = " dollar bills";
+
+		if (hundred == 1) {
+			System.out.println(hundred + "  hundred dollar bill");
+		} else if (hundred >= 1) {
+			System.out.println(hundred + " hundred" + bills);
+		}
+		if (fifty == 1) {
+			System.out.println(fifty + "  fifty dollar bill");
+		} else if (fifty >= 1) {
+			System.out.println(fifty + "  fifty" + bills);
+		}
+		if (twenty == 1) {
+			System.out.println(twenty + "  twenty dollar bill");
+		} else if (twenty >= 1) {
+			System.out.println(twenty + "  twenty" + bills);
+		}
+		if (ten == 1) {
+			System.out.println(ten + "  ten dollar bill");
+		} else if (ten >= 1) {
+			System.out.println(ten + " ten" + bills);
+		}
+		if (five == 1) {
+			System.out.println(five + "  five dollar bill");
+		} else if (five >= 1) {
+			System.out.println(five + " five" + bills);
+		}
+		if (one == 1) {
+			System.out.println(one + "  one dollar bill");
+		} else if (one >= 1) {
+			System.out.println(one + " one" + bills);
+		}
+		if (quarter == 1) {
+			System.out.println(quarter + "  quarter");
+		} else if (quarter >= 1) {
+			System.out.println(quarter + " quarters");
+		}
+		if (dime == 1) {
+			System.out.println(dime + "  dime");
+		} else if (dime >= 1) {
+			System.out.println(dime + "  dimes");
+		}
+		if (nickel == 1) {
+			System.out.println(nickel + "  nickel");
+		} else if (nickel >= 1) {
+			System.out.println(nickel + " nickels");
+		}
+		if (penny == 1) {
+			System.out.println(penny + "  penny");
+		} else if (penny >= 1) {
+			System.out.println(penny + "  pennies");
+		}
 	}
 
-//	public static void changeMaker() {
-//		double dollar = 1;
-//		double price = .56;//1 quarter, 1 dime, 1 nickel, 4 pennies
-//		double change = 0;
-//		if (dollar > price) {
-//			change = dollar - price;
-//			System.out.println(change);
-//		}
-
-	}
-//	User Story #4
-//	If the amount tendered is more than the cost of the item, display the 
-//	number of bills and coins that should be given to the customer.
+}
